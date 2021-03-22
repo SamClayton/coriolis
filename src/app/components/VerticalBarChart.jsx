0d8cdf98f8db93d8be8673802a320419a6831609
Sam Clayton <sam@goranku.com>
2021-03-22 01:44:36 -0700

    Import PropTypes from prop-types rather than react because it's deprecated and breaks the app in React 16.x. Fixes #1.

--- src/app/components/VerticalBarChart.jsx
+++ src/app/components/VerticalBarChart.jsx
@@ -1,5 +1,6 @@
 import TranslatedComponent from './TranslatedComponent';
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ContainerDimensions from 'react-container-dimensions';
 import { BarChart, Bar, XAxis, YAxis, LabelList } from 'recharts';
 
